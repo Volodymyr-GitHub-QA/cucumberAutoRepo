1432fb7eca7b068d6ee9c6469acdb2945d8cb9c0
VTurskyi <VTurskyi@VTurskyi-NB.luxoft.com>
2021-01-19 23:25:52 +0200

    Second commit

--- CucumberAuto/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
+++ CucumberAuto/src/test/java/com/basic/FirstFeatureFile/MyFirstStepDef.java
@@ -19,6 +19,7 @@ public class MyFirstStepDef {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\VTurskyi\\Documents\\Work\\AutomationTesting\\source\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("https://www.facebook.com");
+		System.out.print("Opening URL");
 	}
 	
 	@When("^User enters user first name$")
